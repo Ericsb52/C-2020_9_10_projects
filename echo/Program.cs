ee54e95c932b800b1a9d2cda17578c66fd0815b6
ebroadbent <broadbent52@aol.com>
2020-09-16 14:06:12 -0600

    echo activity 9/16/2020

---
+++ echo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Eric Broadbent
+Hello World example
+ch 2.3
+9/20
+*/
+
+
+
+namespace Hello
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("type in your Name");// user input prompt
+            string name = Console.ReadLine();//Reading user input and assigning it to name
+
+            string myFirstVarable; //declaration statment 
+            myFirstVarable = "Welcome";// asignment statment
+
+
+            string Var2 = "somthing else"; //declaration and assignment in one
+            
+
+            Console.WriteLine(myFirstVarable + " " + name);// this is a console print statment
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();// reading key presses
+        }
+    }
+}
